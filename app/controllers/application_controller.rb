ded67d693ecc5293febbf18911d68193773b9416
Renan Diego <renan.ccomp@gmail.com>
2023-11-12 14:07:39 -0300

    Expose methods with helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find_by(id: session[:user_id])
   end
+  helper_method :current_user
 
   def login(user)
     Current.user = user
@@ -15,6 +16,7 @@ class ApplicationController < ActionController::Base
   def user_signed_in?
     current_user.present?
   end
+  helper_method :user_signed_in?
 
   def logout(user)
     Current.user = nil
